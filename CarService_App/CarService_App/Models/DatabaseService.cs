be8eb9bfc97509122c9c037d5af073e1582c7497
Siil777 <pavel.ivanov.thk@gmail.com>
2024-01-12 09:42:31 +0200

    Add project files.

---
+++ CarService_App/CarService_App/Models/DatabaseService.cs
@@ -0,0 +1,127 @@
+﻿using CarService_App.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarService_App.Services
+{
+    public class DatabaseService
+    {
+        private SQLiteConnection database;
+
+        public DatabaseService(string databasePath)
+        {
+            database = new SQLiteConnection(databasePath);
+            database.CreateTable<Car>();
+        }
+
+        public IEnumerable<Car> GetCars()
+        {
+            try
+            {
+                if (database == null)
+                {
+                    throw new Exception("Database is not initialized. Call Initialize method first.");
+                }
+
+                return database.Table<Car>().ToList();
+            }
+            catch (Exception ex)
+            {
+            
+                Console.WriteLine($"Error getting cars from the database: {ex.Message}");
+                throw;
+            }
+        }
+
+        public List<Car> GetCarAll()
+        {
+            try
+            {
+                // _database.Table<Car>() returns all cars in the table
+                return database.Table<Car>().ToList();
+            }
+            catch (Exception ex)
+            {
+             
+                Console.WriteLine($"Error getting cars from the database: {ex.Message}");
+                throw new Exception($"Error getting cars from the database: {ex.Message}", ex);
+            }
+        }
+        public Car AddOrUpdateCar(Car car)
+        {
+            try
+            {
+                var existingCar = GetCarById(car.CarId);
+
+                if (existingCar == null)
+                {
+                    // New car, add to the collection and insert into the database
+                    InsertCar(car);
+                }
+                else
+                {
+                    // Existing car, update the collection and database
+                    existingCar.Brand = car.Brand;
+                    existingCar.Model = car.Model;
+                    existingCar.Year = car.Year;
+                    existingCar.Description = car.Description;
+                    existingCar.ImagePath = car.ImagePath;
+
+                    UpdateCar(existingCar);
+                }
+
+                return car;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error adding or updating car: {ex.Message}");
+                return null; // Indicates failure
+            }
+        }
+
+
+
+
+        public Car InsertCar(Car car)
+        {
+            try
+            {
+                database.Insert(car);
+                return car; // Indicates successful insertion
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error inserting car into the database: {ex.Message}");
+                return null; // Indicates failure
+            }
+        }
+
+        public Car UpdateCar(Car car)
+        {
+            try
+            {
+                database.Update(car);
+                return car; // Indicates successful update
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error updating car in the database: {ex.Message}");
+                return null; // Indicates failure
+            }
+        }
+
+
+
+        public Car GetCarById(int carId)
+        {
+            return database.Table<Car>().FirstOrDefault(c => c.CarId == carId);
+        }
+    }
+
+
+}
+
+
+
---
+++ CarService_App/CarService_App/Views/CarServicePage.xaml.cs
@@ -0,0 +1,64 @@
+﻿// CarServicePage.xaml.cs
+using Xamarin.Forms;
+using CarService_App.Models;
+using CarService_App.ViewModels;
+using CarService_App.Services;
+using System;
+using System.IO;
+
+namespace CarService_App.Views
+{
+    public partial class CarServicePage : ContentPage
+    {
+        public CarServicePage()
+        {
+            InitializeComponent();
+            opacityImage.Opacity = 0.25;
+           
+        }
+       
+
+        protected override void OnAppearing()
+        {
+            // Refresh the list of cars when the page appears
+            // to get all cars
+            CarsList.ItemsSource = App.Database.GetCarAll();
+
+            base.OnAppearing();
+        }
+
+
+        private async void OnCarSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem is Car selectedCar)
+            {
+                await Navigation.PushAsync(new CarDetailsPage(new CarDetailsViewModel(
+                selectedCar.CarId,          // Pass carId
+                selectedCar.Brand,
+                selectedCar.Model,           // Pass model
+                selectedCar.Year,            // Pass year
+                selectedCar.Description,
+                selectedCar.ImagePath
+            )));
+
+            }
+
+            ((ListView)sender).SelectedItem = null; // Clear selection
+        }
+        private async void CreateCar(object sender, EventArgs e)
+        {
+            // Create an instance of CarDetailsViewModel for adding a new car
+            CarDetailsViewModel viewModel = new CarDetailsViewModel();
+
+            // Create a new instance of CarDetailsPage and pass the viewModel
+            CarDetailsPage CarPage = new CarDetailsPage(viewModel);
+
+            // Set the BindingContext of the page to the viewModel
+            CarPage.BindingContext = viewModel;
+
+            // Navigate to CarDetailsPage for adding a new car
+            await Navigation.PushAsync(CarPage);
+        }
+    }
+}
+
